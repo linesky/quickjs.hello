08295563a92f6503351bcbf83433b01d5e63f203
sky line <32512284+linesky@users.noreply.github.com>
2024-10-30 21:33:53 +0000

    Delete opcode.sh

--- opcode.sh
+++
@@ -1,9 +0,0 @@
-echo "give me a opcode"
-while read ln
-do
-    echo "give me a opcode"
-    echo "org 0x100" > /tmp/out.asm
-    echo $ln >> /tmp/out.asm
-    nasm /tmp/out.asm -o /tmp/out
-    xxd /tmp/out
-done
