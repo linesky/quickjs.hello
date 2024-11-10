5c94565730604c3116be173cca6aa45e147fe99e
sky line <32512284+linesky@users.noreply.github.com>
2024-11-10 18:10:13 +0000

    Add files via upload

---
+++ jjs.js
@@ -0,0 +1,14 @@
+function add( a, b){
+    var out=a+b;
+    return out;
+}
+function Main() {
+        var f=10.9;
+        f=f+10;
+        var s="hello world...";
+        s=s+".......";
+        print(s);
+        print(f);
+        return f;
+}
+Main();
